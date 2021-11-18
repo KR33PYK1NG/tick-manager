1a54dca19b05db24dd3e0e263799f5878a59985a
KR33PY <kr33pyrmc@gmail.com>
2021-11-18 07:46:41 +0300

    ChunkMcAPI встроен в тикменеджер в связи с переходом на Arclight

--- src/main/java/rmc/mixins/tick_manager/actual/ChunkManagerMixin.java
+++ src/main/java/rmc/mixins/tick_manager/actual/ChunkManagerMixin.java
@@ -1,5 +1,6 @@
 package rmc.mixins.tick_manager.actual;
 
+import net.minecraft.world.server.ChunkHolder;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
@@ -13,11 +14,17 @@ import rmc.mixins.tick_manager.extend.ChunkManagerEx;
 public abstract class ChunkManagerMixin
 implements ChunkManagerEx {
 
+    @Shadow protected ChunkHolder func_219219_b(long chunkPosIn) { return null; }
     @Shadow protected void tickEntityTracker() {}
 
     @Override
     public void rmc$tickEntityTracker() {
         this.tickEntityTracker();
+    }
+
+    @Override
+    public ChunkHolder rmc$getChunkHolder(long l) {
+        return this.func_219219_b(l);
     }
 
 }
--- src/main/java/rmc/mixins/tick_manager/extend/ChunkManagerEx.java
+++ src/main/java/rmc/mixins/tick_manager/extend/ChunkManagerEx.java
@@ -1,10 +1,14 @@
 package rmc.mixins.tick_manager.extend;
 
+import net.minecraft.world.server.ChunkHolder;
+
 /**
  * Developed by RMC Team, 2021
  */
 public interface ChunkManagerEx {
 
     public void rmc$tickEntityTracker();
+
+    public ChunkHolder rmc$getChunkHolder(long l);
 
 }
